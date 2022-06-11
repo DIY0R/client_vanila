47e46463f472ee27329eae98ef25f039d25e5a32
diyor <sabirovdiyorkind@gmail.com>
2022-06-11 19:08:46 +0500

    change syntactic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { Authorization } from '../core/Authorization/authorization'
-import { useCaseAuth } from '../core/Authorization/useCaseAuth'
-import { AuthorizationRealization } from './bll/auth/authorization'
 import { App } from './components/app'
 import { Footer } from './components/footer/footer'
 import { Header } from './components/header/header'
@@ -9,7 +6,6 @@ import { Main } from './components/main/main'
 import { RegisterListeners } from './routes/registerListeners'
 import { Route } from './routes/Route'
 import { routesInfo } from './routes/routesInfo'
-
 
 export const app = new App('.app', [
   new Header(new Route(), routesInfo),
