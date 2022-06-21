2ddf4920044cea05cbfae6234adaeb827661a8cb
diyor <sabirovdiyorkind@gmail.com>
2022-06-22 00:00:00 +0500

    files for container

---
+++ src/bll/auth/config/typesContainer.ts
@@ -0,0 +1,9 @@
+import { returnLoginInterface } from '../../../../core/Authorization/types/returnLoginInterface'
+
+export interface AuthUseAdapterInterface {
+  login(name: string, email: string): Promise<returnLoginInterface>
+  get getIsAuth(): boolean
+}
+export const TYPESContainer = {
+  authBll: Symbol.for('authBll'),
+}
---
+++ src/components/main/Registration/config/typeContainer.ts
@@ -0,0 +1,4 @@
+export const TYPESContainer = {
+  loginActions: Symbol.for('loginActions'),
+  loginPage: Symbol.for('loginPage'),
+}
