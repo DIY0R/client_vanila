bf1af9a0c51469067ebb400347021ae8828c6dd9
diyor <sabirovdiyorkind@gmail.com>
2022-06-21 23:58:54 +0500

    chnage types and imports

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import { Main } from './components/main/main'
 import { RegisterListeners } from './routes/registerListeners'
 import { Route } from './routes/Route'
 import { routesInfo } from './routes/routesInfo'
-
+import './inversify.config'
+// import './bll/auth/config/auth.container'
 export const app = new App('.app', [
   new Header(new Route(), routesInfo),
   new Main(new RegisterListeners(routesInfo)),
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export const TYPESContainer = {
-  loginActions: Symbol.for('loginActions'),
-  loginPage: Symbol.for('loginPage'),
-  authBll: Symbol.for('authBll'),
-}
